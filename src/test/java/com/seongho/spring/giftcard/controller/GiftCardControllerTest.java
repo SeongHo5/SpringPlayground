262d82e3e25130f06750cd888b42c370dfd390ba
SeongHo5 <newfuring1123@gmail.com>
2024-01-06 18:33:04 +0900

    refactor: Refactor test code using DynamicTest

--- src/test/java/com/seongho/spring/giftcard/controller/GiftCardControllerTest.java
+++ src/test/java/com/seongho/spring/giftcard/controller/GiftCardControllerTest.java
@@ -1,13 +1,19 @@
 package com.seongho.spring.giftcard.controller;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Stream;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -21,107 +27,63 @@ class GiftCardControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    @DisplayName("상품권 발급 성공")
+    @TestFactory
+    @DisplayName("상품권 사용 테스트")
     @Transactional
-    void shouldIssueGiftCard() throws Exception {
-        // Given
-        String value = "10000";
-
-        // When & Then
-        mockMvc.perform(post("/api/gift-card/issue")
-                .param("value", value))
-                .andExpect(status().isOk());
+    Stream<DynamicTest> dynamicTestForUseGiftCard() {
+        return Stream.of(
+                testApiEndpoint("상품권 사용 성공", useGiftCard("CLLW-2QQ4-QY4A-PWZ6W9"), status().isOk()),
+                testApiEndpoint("상품권 사용 실패 - 존재하지 않는 코드", useGiftCard("1111-2222-3333-444444"), status().isNotFound()),
+                testApiEndpoint("상품권 사용 실패 - 이미 사용된 상품권", useGiftCard("A9KM-0USD-FW5V-2VPXO5"), status().is4xxClientError()),
+                testApiEndpoint("상품권 사용 실패 - 만료된 상품권", useGiftCard("G4UX-AKWG-LEUV-5NZ278"), status().is4xxClientError())
+        );
     }
 
-    @Test
-    @DisplayName("상품권 발급 실패 - 최소 금액 미만")
-    @Transactional
-    void shouldFailToIssueGiftCardUnderMinimumValue() throws Exception {
-        // Given
-        String value = "9999";
-
-        // When & Then
-        mockMvc.perform(post("/api/gift-card/issue")
-                .param("value", value))
-                .andExpect(status().isBadRequest());
+    private RequestBuilder useGiftCard(String code) {
+        return post("/api/gift-card/use").param("code", code);
     }
 
-    @Test
-    @DisplayName("상품권 발급 실패 - 최대 금액 초과")
+    @TestFactory
+    @DisplayName("상품권 발급 테스트")
     @Transactional
-    void shouldFailToIssueGiftCardOverMaximumValue() throws Exception {
-        // Given
-        String value = "10000001";
-
-        // When & Then
-        mockMvc.perform(post("/api/gift-card/issue")
-                .param("value", value))
-                .andExpect(status().isBadRequest());
+    Stream<DynamicTest> dynamicTestForIssueGiftCard() {
+        return Stream.of(
+                testApiEndpoint("상품권 발급 성공", issueGiftCard("10000"), status().isOk()),
+                testApiEndpoint("상품권 발급 실패 - 최소 금액 미만", issueGiftCard("9999"), status().isBadRequest()),
+                testApiEndpoint("상품권 발급 실패 - 최대 금액 초과", issueGiftCard("10000001"), status().isBadRequest())
+        );
     }
 
-    @Test
-    @DisplayName("상품권 사용 성공")
-    @Transactional
-    void shouldUseGiftCard() throws Exception {
-        // Given
-        String code = "CLLW-2QQ4-QY4A-PWZ6W9";
-
-        // When & Then
-        mockMvc.perform(post("/api/gift-card/use")
-                .param("code", code))
-                .andExpect(status().isOk());
+    private RequestBuilder issueGiftCard(String value) {
+        return post("/api/gift-card/issue").param("value", value);
     }
 
-    @Test
-    @DisplayName("상품권 사용 실패 - 존재하지 않는 코드")
+    @TestFactory
+    @DisplayName("상품권 폐기 테스트")
     @Transactional
-    void shouldFailToUseGiftCardWithNonExistentCode() throws Exception {
-        // Given
-        String code = "1111-2222-3333-444444";
-
-        // When & Then
-        mockMvc.perform(post("/api/gift-card/use")
-                .param("code", code))
-                .andExpect(status().isNotFound());
+    Stream<DynamicTest> dynamicTestForDisposeGiftCard() {
+        return Stream.of(
+                testApiEndpoint("상품권 폐기 성공", disposeGiftCard("CLLW-2QQ4-QY4A-PWZ6W9"), status().isOk()),
+                testApiEndpoint("상품권 폐기 실패 - 존재하지 않는 코드", disposeGiftCard("1111-2222-3333-444444"), status().isNotFound())
+        );
     }
 
-    @Test
-    @DisplayName("상품권 사용 실패 - 이미 사용된 상품권")
-    @Transactional
-    void shouldFailToUseAlreadyUsedGiftCard() throws Exception {
-        // Given
-        String code = "A9KM-0USD-FW5V-2VPXO5";
-
-        // When & Then
-        mockMvc.perform(post("/api/gift-card/use")
-                        .param("code", code))
-                .andExpect(status().is4xxClientError());
+    private RequestBuilder disposeGiftCard(String code) {
+        return delete("/api/gift-card/dispose").param("code", code);
     }
 
-    @Test
-    @DisplayName("상품권 사용 실패 - 만료된 상품권")
-    @Transactional
-    void shouldFailToUseExpiredGiftCard() throws Exception {
-        // Given
-        String code = "G4UX-AKWG-LEUV-5NZ278";
-
-        // When & Then
-        mockMvc.perform(post("/api/gift-card/use")
-                .param("code", code))
-                .andExpect(status().is4xxClientError());
+    /**
+     * 지정된 API 엔드포인트에 대해 테스트를 수행하는 DynamicTest를 생성합니다.
+     *
+     * @param testName 테스트의 Display Name으로 사용될 이름
+     * @param requestBuilder API 엔드포인트에 보낼 요청, HTTP Method, URL, 매개변수 또는 본문이 포함되어야 합니다.
+     * @param expectedStatus API 엔드포인트의 응답으로 예상되는 HTTP 상태
+     * @return DynamicTest
+     */
+    private DynamicTest testApiEndpoint(String testName, RequestBuilder requestBuilder, ResultMatcher expectedStatus) {
+        return DynamicTest.dynamicTest(testName,
+                () -> mockMvc.perform(requestBuilder)
+                        .andExpect(expectedStatus));
     }
 
-    @Test
-    @DisplayName("상품권 폐기 성공")
-    @Transactional
-    void shouldDisposeGiftCard() throws Exception {
-        // Given
-        String code = "CLLW-2QQ4-QY4A-PWZ6W9";
-
-        // When & Then
-        mockMvc.perform(delete("/api/gift-card/dispose")
-                .param("code", code))
-                .andExpect(status().isOk());
-    }
 }
