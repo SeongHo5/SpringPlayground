578990fa767d33088c3d39a97a1cf2dc7e70b0c8
SeongHo5 <newfuring1123@gmail.com>
2024-01-07 13:24:21 +0900

    feat: Add permission features
    - 관리자만 API 접근 가능하도록 권한 기능 추가

--- src/main/java/com/seongho/spring/giftcard/controller/GiftCardController.java
+++ src/main/java/com/seongho/spring/giftcard/controller/GiftCardController.java
@@ -2,11 +2,13 @@ package com.seongho.spring.giftcard.controller;
 
 import com.seongho.spring.giftcard.service.GiftCardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/gift-card")
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 public class GiftCardController {
 
     private final GiftCardService giftCardService;
